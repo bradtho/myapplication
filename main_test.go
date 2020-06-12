fa7a570c1100c95c037d2bce764bd150fec4cbda
Brad Thomas <brad.thomas@digio.com.au>
2020-06-12 13:07:42 +1000

    fix returning false negatives breaking build pipeline

--- main_test.go
+++ main_test.go
@@ -25,18 +25,18 @@ func TestVersionHandler(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Returned unexpected status code: \n\tgot %v \n\texpected %v",
 			status, http.StatusOK)
-	} else {
-		t.Logf("Returned expected status code: \n\tgot %v \n\texpected %v",
-			status, http.StatusOK)
-	}
+	} //else {
+	//t.Logf("Returned expected status code: \n\tgot %v \n\texpected %v",
+	//status, http.StatusOK)
+	//}
 
 	// Check the body is what we expect.
 	expected := `{"myapplication":[{"version":"dev","lastcommitsha":"xyz7890","description":"pre-interview technical test"}]}`
 	if strings.TrimRight(rr.Body.String(), "\n") != expected {
 		t.Errorf("Returned unexpected body: \n\tgot %v \n\texpected %v",
 			rr.Body.String(), expected)
-	} else {
-		t.Logf("Returned expected body: \n\tgot %v \n\texpected %v",
-			rr.Body.String(), expected)
-	}
+	} //else {
+	//t.Logf("Returned expected body: \n\tgot %v \n\texpected %v",
+	//rr.Body.String(), expected)
+	//}
 }
